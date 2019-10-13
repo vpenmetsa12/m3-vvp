5b7deabf1a98d74c0209d830fc76dd5c12ec05c0
Vinay Penmetsa <vpenmetsa12@gmail.com>
2019-10-12 22:48:48 -0400

    Added comments in home.page.ts file

--- milestone3vvp/src/app/home/home.page.ts
+++ milestone3vvp/src/app/home/home.page.ts
@@ -6,24 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // Squat Variables
   squat_time = 0;
   squat_click = false;
   squat_set = 0;
   avg_squat_time = 0;
   squat_timer_disabled = false;
   squat_end_disabled = true;
+  // bench variables
   bench_time = 0;
   bench_click = false;
   bench_set = 0;
   avg_bench_time = 0;
   bench_timer_disabled = false;
   bench_end_disabled = true
+  // deadlift variabbles
   dl_time = 0;
   dl_click = false;
   dl_set = 0;
   avg_dl_time = 0;
   dl_timer_disabled = false;
   dl_end_disabled = true
+  // Starts squat timer
   squatTimer() { 
     this.squat_click = true;
     this.squat_timer_disabled = true;
@@ -39,11 +43,13 @@ export class HomePage {
       }
     }, 1000);
   }
+  // Ends squat timer
   endSquatTimer() {
     this.squat_timer_disabled = false;
     this.squat_click = false;
     this.squat_end_disabled = true;
   }
+  // Resets all variables
   clearSquatTimer() {
     this.squat_time = 0;
     this.squat_click = false;
@@ -52,6 +58,7 @@ export class HomePage {
     this.squat_timer_disabled = false;
     this.squat_end_disabled = true;
   }
+  // starts bench timer
   benchTimer() { 
     this.bench_click = true;
     this.bench_timer_disabled = true;
@@ -67,11 +74,13 @@ export class HomePage {
       }
     }, 1000);
   }
+  // ends bench timer
   endBenchTimer() {
     this.bench_timer_disabled = false;
     this.bench_click = false;
     this.bench_end_disabled = true;
   }
+  // resets all variables
   clearBenchTimer() {
     this.bench_time = 0;
     this.bench_click = false;
@@ -80,6 +89,7 @@ export class HomePage {
     this.bench_timer_disabled = false;
     this.bench_end_disabled = true;
   }
+  // starts bench timer
   DLTimer() { 
     this.dl_click = true;
     this.dl_timer_disabled = true;
@@ -95,11 +105,13 @@ export class HomePage {
       }
     }, 1000);
   }
+  // ends bench timer
   endDLTimer() {
     this.dl_timer_disabled = false;
     this.dl_click = false;
     this.dl_end_disabled = true;
   }
+  // clears all bench variables
   clearDLTimer() {
     this.dl_time = 0;
     this.dl_click = false;
